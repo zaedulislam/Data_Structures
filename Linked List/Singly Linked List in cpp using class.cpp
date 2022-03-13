609cf4c4ed1a2dfab9b978ab3785c6240f8ec654
Zaedul Islam <islam.zaedul@gmail.com>
2022-03-12 22:39:08 -0500

    Update Singly Linked List in cpp using class.cpp

--- Linked List/Singly Linked List in cpp using class.cpp
+++ Linked List/Singly Linked List in cpp using class.cpp
@@ -1,1 +1,47 @@
 
+#include <iostream>
+using namespace  std;
+
+struct Node{
+    int data;
+    Node *next;
+};
+
+class SinglyLinkedList{
+    private:
+        Node* head;
+        int linkedListLength;
+
+    public:
+        SinglyLinkedList();
+
+        void insertAtBeginning(int value);
+        void insertAtMiddle(int value);
+        void insertAtEnd(int value);
+        void insertAtGivenPosition(int value);
+
+        void deleteFromBeginning(int value);
+        void deleteFromMiddle(int value);
+        void deleteFromEnd(int value);
+        void deleteFromGivenPosition(int value);
+
+        void printSinglyLinkedLis();
+};
+
+
+SinglyLinkedList::SinglyLinkedList() {
+    head = NULL;
+    linkedListLength = 0;
+}
+
+
+
+int main() {
+    SinglyLinkedList linkedListObject;
+
+    linkedListObject.printSinglyLinkedLis();
+
+
+
+    return 0;
+}
