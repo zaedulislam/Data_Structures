cf2fa3ce2485ceccf7cc3ac5ec94fb67e383ab7f
Zaedul Islam <islam.zaedul@gmail.com>
2025-04-24 00:12:54 -0400

    Update disjoint set

--- Disjoint Set/DisjointSet.java
+++ Disjoint Set/DisjointSet.java
@@ -2,16 +2,16 @@
  * This implementation of the “disjoint set” is optimized with both “path compression” and “union by rank”.
  */
 class DisjointSet {
-    private int[] parent;
+    private int[] root;
     private int[] rank;
 
     public DisjointSet(int size) {
-        parent = new int[size];
+        root = new int[size];
         // Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
         rank = new int[size];
 
         for (int i = 0; i < size; i++) {
-            parent[i] = i;
+            root[i] = i;
             // The initial "rank" of each vertex is 1, because each of them is
             // a standalone vertex with no connection to other vertices.
             rank[i] = 1;
@@ -20,27 +20,27 @@ class DisjointSet {
 
     // The find function here is the same as that in the disjoint set with path compression.
     public int find(int u) {
-        if (u == parent[u]) {
+        if (u == root[u]) {
             return u;
         }
 
         // Some ranks may become obsolete so they are not updated
-        return parent[u] = find(parent[u]);
+        return root[u] = find(root[u]);
     }
 
     // The union function with union by rank
     public void union(int u, int v) {
-        int parentU = find(u);
-        int parentV = find(v);
+        int rootU = find(u);
+        int rootV = find(v);
 
-        if (parentU != parentV) {
-            if (rank[parentU] > rank[parentV]) {
-                parent[parentV] = parentU;
-            } else if (rank[parentU] < rank[parentV]) {
-                parent[parentU] = parentV;
+        if (rootU != rootV) {
+            if (rank[rootU] > rank[rootV]) {
+                root[rootV] = rootU;
+            } else if (rank[rootU] < rank[rootV]) {
+                root[rootU] = rootV;
             } else {
-                parent[parentV] = parentU;
-                rank[parentU] += 1;
+                root[rootV] = rootU;
+                rank[rootU] += 1;
             }
         }
     }
@@ -58,11 +58,11 @@ class DisjointSet {
         disjointSet.union(6, 7);
         disjointSet.union(3, 8);
         disjointSet.union(8, 9);
-        System.out.println(uf.connected(1, 5)); // true
-        System.out.println(uf.connected(5, 7)); // true
-        System.out.println(uf.connected(4, 9)); // false
+        System.out.println(disjointSet.connected(1, 5)); // true
+        System.out.println(disjointSet.connected(5, 7)); // true
+        System.out.println(disjointSet.connected(4, 9)); // false
         // 1-2-5-6-7 3-8-9-4
         disjointSet.union(9, 4);
-        System.out.println(uf.connected(4, 9)); // true
+        System.out.println(disjointSet.connected(4, 9)); // true
     }
 }
